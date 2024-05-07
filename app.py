58baa509c21d455b328cfa24cddc052a014a2377
Amitvik <amitvikram299@gmail.com>
2024-05-06 21:57:51 -0500

    -Added local histogram equalization changes for color images

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
+import io
+
+
 from flask import Flask, request, send_file
 import logging
 from io import BytesIO
 import numpy as np
-from globalHist import globalHistEqual
-from localHist import localHistEqual4e, localHistColor
+
+from globalHist import globalHistEqual, rgb_to_hsi_global, equalize_intensity, hsi_to_rgb_global
+from localHist import local_hist_equalization, local_hist_color
 from PIL import Image
+
+from processColorImage import hsi_to_rgb, rgb_to_hsi
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-def invert_pixels(img_data):
-    inverted_img_data = bytearray([255 - byte for byte in img_data])
-    return inverted_img_data
 
 
 @app.route('/image-local', methods=['POST'])
@@ -27,9 +28,10 @@ def process_local_image():
     image_file = request.files['image']
 
     input_image = np.array(Image.open(image_file))
-    output = localHistEqual4e(input_image, 3, 3)
+    output = local_hist_equalization(input_image, 3, 3)
 
     output_image = Image.fromarray(output)
+    output_image.save("processed_image.png")
     output_data = BytesIO()
     output_image.save(output_data, format='PNG')
     output_data.seek(0)
@@ -49,6 +51,8 @@ def process_global_image():
     output = globalHistEqual(input_image)
 
     output_image = Image.fromarray(output)
+    output_image.save("global_hist.png")
+
     output_data = BytesIO()
     output_image.save(output_data, format='PNG')
     output_data.seek(0)
@@ -67,15 +71,44 @@ def process_image_color():
     image_file = request.files['image']
 
     input_image = np.array(Image.open(image_file))
-    output = localHistColor(input_image)
+    output = local_hist_color(input_image)
 
     output_image = Image.fromarray(output)
+    output_image.save("processed_image.png")
     output_data = BytesIO()
+    print("saving file")
     output_image.save(output_data, format='PNG')
     output_data.seek(0)
 
     return send_file(output_data, mimetype='image/png', as_attachment=True,
                      download_name='local_histogram_equalized_image.png')
+
+
+@app.route('/image-global-color', methods=['POST'])
+def image_global_color():
+    # Check if the post request has the image part
+    if 'image' not in request.files:
+        return "No image part", 400
+    image_file = request.files['image']
+    if image_file.filename == '':
+        return "No selected image", 400
+    if image_file:
+        # Read the image file
+        img = Image.open(image_file.stream).convert('RGB')
+        rgb = np.array(img)
+        hsi = rgb_to_hsi_global(rgb)
+        hsi = equalize_intensity(hsi)
+        processed_rgb = hsi_to_rgb_global(hsi)
+        processed_image = Image.fromarray(np.uint8(processed_rgb))
+
+        # Save processed image to a bytes buffer
+        img_byte_arr = io.BytesIO()
+        processed_image.save(img_byte_arr, format='PNG')
+        img_byte_arr.seek(0)
+
+        return send_file(img_byte_arr, mimetype='image/png')
+
+    return "Unsupported image type", 400
 
 
 if __name__ == '__main__':
